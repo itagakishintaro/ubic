2fbb37bda9978844564ff72bcb2b37d919bf1ddd
itagaki <itagaki.shintaro@gmail.com>
2015-05-24 13:27:56 +0900

    msg

--- public/js/ubic.js
+++ public/js/ubic.js
@@ -2,14 +2,21 @@
 var USER_NUM = $('tr').length - 1;
 
 $('#ubic-document-btn').on('click', function(){
-	var userData = [];
 	for (var i = 1; i <= USER_NUM; i++) {
-		userData[i] = '';
-		userData[i] += $('#' + i + ' .mail').text();
-		userData[i] += $('#' + i + ' .twitter').text();
-		userData[i] += $('#' + i + ' .qiita').text();
-		userData[i] += $('#' + i + ' .rec-text').text();
-		console.log(userData[i]);
+		var text = '';
+		text += $('#' + i + ' .mail').text();
+		text += $('#' + i + ' .twitter').text();
+		text += $('#' + i + ' .qiita').text();
+		text += $('#' + i + ' .rec-text').text();
+		console.log(text);
+		var data = {
+			"documentId": i,
+			"categoryId": 401,
+			"text": text
+		};
+		$.post('/api/ubic/document', data, function(result){
+			console.log(result);
+		});
 	}
 });
 
